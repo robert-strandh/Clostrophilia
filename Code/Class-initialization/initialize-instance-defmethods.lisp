fb0351f73178a02eadaa2412578757d4f656ce96
Robert Strandh <robert.strandh@gmail.com>
2023-12-18 14:56:01 +0100

    Improve code and comments.

--- Code/Class-initialization/initialize-instance-defmethods.lisp
+++ Code/Class-initialization/initialize-instance-defmethods.lisp
@@ -1,16 +1,20 @@
 (cl:in-package #:clostrophilia)
 
-;;;; The AMOP says that when a class is initialized, then if the
-;;;; :DIRECT-SUPERCLASSES keyword argument is either not given or it
-;;;; is the empty list, then it default depending on the class being
-;;;; initialized.  We do that by calling another generic function
-;;;; DEFAULT-SUPERCLASSES with CLASS as its argument.
+;;; The AMOP says that when a class is initialized, then if the
+;;; :DIRECT-SUPERCLASSES keyword argument is either not given or it is
+;;; the empty list, then it default depending on the class being
+;;; initialized.  We do that by calling another generic function
+;;; DEFAULT-SUPERCLASSES with CLASS as its argument.
 
 (defmethod initialize-instance :around
     ((class class) &rest initargs &key direct-superclasses &allow-other-keys)
-  (if (null direct-superclasses)
-      (apply #'call-next-method
-             class
-             :direct-superclasses (default-superclasses class)
-             initargs)
-      (call-next-method)))
+  (let ((defaulted-direct-superclasses
+          (if (null direct-superclasses)
+              (default-superclasses class)
+              direct-superclasses)))
+    (loop for defaulted-direct-superclass in defaulted-direct-superclasses
+          do (add-direct-subclass defaulted-direct-superclass class))
+    (apply #'call-next-method
+           class
+           :direct-superclasses defaulted-direct-superclasses
+           initargs)))
