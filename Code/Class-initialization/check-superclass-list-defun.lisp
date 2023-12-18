2663ad1efaef8f9e164eefac947435a901529adc
Robert Strandh <robert.strandh@gmail.com>
2023-12-18 18:14:20 +0100

    New coponent containing code for checking superclass list.

---
+++ Code/Class-initialization/check-superclass-list-defun.lisp
@@ -0,0 +1,15 @@
+(cl:in-package #:clostrophilia)
+
+(defun check-superclass-list (class superclasses)
+  (unless (ecclesia:proper-list-p superclass-list)
+    (error 'superclasses-must-be-proper-list
+           :superclasses superclasses))
+  (loop for superclass in superclasses
+        do (unless (typep superclass 'class)
+             (error 'type-error
+                    :datum superclass
+                    :expected-type 'class))
+           (unless (validate-superclass class superclass)
+             (error 'class-can-not-be-superclass
+                    :class class
+                    :superclass superclass))))
--- Code/Class-initialization/clostrophilia-class-initialization.asd
+++ Code/Class-initialization/clostrophilia-class-initialization.asd
@@ -1,11 +1,13 @@
 (cl:in-package #:asdf-user)
 
 (defsystem "clostrophilia-class-initialization"
+  :depends-on ("ecclesia")
   :serial t
   :components
   ((:file "remove-direct-subclass-defgeneric")
    (:file "remove-direct-subclass-defmethod")
    (:file "add-direct-subclass-defgeneric")
    (:file "add-direct-subclass-defmethod")
+   (:file "check-superclass-list-defun")
    (:file "initialize-instance-defmethods")
    (:file "reinitialize-instance-defmethods")))
