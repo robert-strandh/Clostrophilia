a365fc8f1b0a86d46c1dcef03e4ec20ef2b28085
Robert Strandh <robert.strandh@gmail.com>
2023-12-19 14:18:32 +0100

    New system for the CLOSTROPHELILA package definition.

---
+++ Code/Package/clostrophilia-package.asd
@@ -0,0 +1,6 @@
+(cl:in-package #:asdf-user)
+
+(defsystem "clostrophilia-package"
+  :serial t
+  :components
+  ((:file "packages")))
---
+++ Code/Package/packages.lisp
@@ -0,0 +1,13 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:clostrophilia
+  (:use #:common-lisp)
+  (:export
+   #:funcallable-standard-object
+   #:funcallable-standard-class
+   #:class-direct-default-initargs
+   #:class-direct-slots
+   #:class-name
+   #:class-direct-superclasses
+   #:class-precedence-list))
+
