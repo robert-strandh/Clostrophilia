2f2fb239618cb42f543346bb62df6f3d2c2682eb
Robert Strandh <robert.strandh@gmail.com>
2023-12-20 08:09:41 +0100

    Export names of all MOP classes and of class accessors.

--- Code/Package/packages.lisp
+++ Code/Package/packages.lisp
@@ -3,11 +3,33 @@
 (defpackage #:clostrophilia
   (:use #:common-lisp)
   (:export
-   #:funcallable-standard-object
+   ;; MOP classes
+   #:class #:standard-class #:built-in-class #:structure-class
+   #:forward-referenced-class
    #:funcallable-standard-class
-   #:class-direct-default-initargs
-   #:class-direct-slots
+   #:standard-object #:function #:funcallable-standard-object
+   #:simple-function
+   #:generic-function #:standard-generic-function
+   #:method #:standard-method
+   #:metaobject
+   #:specializer
+   #:eql-specializer
+   #:method-combination
+   #:slot-definition
+   #:direct-slot-definition
+   #:effective-slot-definition
+   #:standard-slot-definition
+   #:standard-direct-slot-definition
+   #:standard-effective-slot-definition
+   #:standard-reader-method
+   #:standard-writer-method
+   ;; Accessors for class metaobjects. 
    #:class-name
    #:class-direct-superclasses
-   #:class-precedence-list))
-
+   #:class-direct-slots
+   #:class-direct-default-initargs
+   #:class-precedence-list
+   #:class-slots
+   #:class-default-initargs
+   #:class-finalized-p
+   #:class-prototype))
