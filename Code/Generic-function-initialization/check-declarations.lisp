5307fbcbb6b3e3090c8bb89640c76e6913eb5d4b
Robert Strandh <robert.strandh@gmail.com>
2023-12-28 15:49:41 +0100

    Check that each declaration is a valid OPTIMIZE declaration.

--- Code/Generic-function-initialization/check-declarations.lisp
+++ Code/Generic-function-initialization/check-declarations.lisp
@@ -1,8 +1,24 @@
 (cl:in-package #:clostrophilia)
 
-;;; FIXME check that each declaration is legal.
+(defparameter *qualities*
+  '(compilation-speed debug safety space speed))
+
+(defun valid-quality-p (quality)
+  (or (member quality *qualities*)
+      (and (ecclesia:proper-list-p quality)
+           (= (length quality 2))
+           (member (first quality) *qualities*)
+           (typep (second quality) '(integer 0 3)))))
+
+(defun check-generic-function-declaration (declaration)
+  (unless (and (ecclesia:proper-list-p declaration)
+               (eq (first declaration) 'optimize)
+               (every #'valid-quality-p (rest declaration)))
+    (error 'generic-function-declaration-must-be-valid
+           :declaration declaration)))
 
 (defun check-generic-function-declarations (declarations)
   (unless (ecclesia:proper-list-p declarations)
     (error 'generic-function-declarations-must-be-proper-list
-           :declarations declarations)))
+           :declarations declarations))
+  (mapc #'check-generic-function-declaration declarations))
