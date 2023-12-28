f8b41f7a3ae0abf3b7ef5af0edff4e20d0bd6090
Robert Strandh <robert.strandh@gmail.com>
2023-12-28 07:17:05 +0100

    Define ARGUMENT-PRECEDENCE-ORDER-GIVEN-BUT-NOT-LAMBDA-LIST.

--- Code/Generic-function-initialization/condition-types.lisp
+++ Code/Generic-function-initialization/condition-types.lisp
@@ -1,1 +1,17 @@
 (cl:in-package #:clostrophilia)
+
+(defgeneric argument-precedence-order (condition))
+
+(define-condition argument-precedence-order-given-but-not-lambda-list
+    (program-error)
+  ((%argument-precedence-order
+    :initarg :argument-precedence-order
+    :reader argument-precedence-order))
+  (:report (lambda (condition stream)
+             (format stream
+                     "When the keyword argument :ARGUMENT-PRECEDENCE-ORDER~@
+                      is given, then the :LAMBDA-LIST argument must also~@
+                      be given, but only the :ARGUMENT-PRECEDENCE-ORDER~@
+                      was given:~@
+                      ~s"
+                     (argument-precedence-order condition)))))
