35e0d67c57b9a119b51773db2234ddf6db30770d
Robert Strandh <robert.strandh@gmail.com>
2023-12-28 15:33:42 +0100

    Check documentation and declarations in :AROUND method.

--- Code/Generic-function-initialization/initialize-instance-methods.lisp
+++ Code/Generic-function-initialization/initialize-instance-methods.lisp
@@ -1,12 +1,11 @@
 (cl:in-package #:clostrophilia)
 
-(defmethod initialize-instance :before
+(defmethod initialize-instance :around
     ((generic-function generic-function)
      &rest initargs
      &key
-       (lambda-list nil lambda-list-p)
-       (argument-precedence-order nil argument-precedence-order-p)
+       (declarations '())
+       (documentation nil)
      &allow-other-keys)
-  (when (and argument-precedence-order-p (not lambda-list-p))
-    (error 'argument-precedence-order-given-but-not-lambda-list
-           :argument-precedence-order argument-precedence-order )))
+  (check-generic-function-declarations declarations)
+  (check-documentation documentation))
