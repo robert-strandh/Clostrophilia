24a62053b265f3308205756f74d96f0d499c2496
Robert Strandh <robert.strandh@gmail.com>
2023-12-30 16:29:24 +0100

    Bind *EXISTING-SUPERCLASSES* around CALL-NEXT-METHOD.

--- Code/Class-initialization/initialize-instance-defmethods.lisp
+++ Code/Class-initialization/initialize-instance-defmethods.lisp
@@ -21,36 +21,28 @@
        (direct-superclasses '())
        (documentation nil)
      &allow-other-keys)
-  (check-direct-default-initargs direct-default-initargs)
-  (check-superclass-list class direct-superclasses)
   (let ((defaulted-direct-superclasses
           (if (null direct-superclasses)
               (default-superclasses class)
-              direct-superclasses))
-        (direct-slot-definitions
-          (check-and-convert-direct-slot-specifications class direct-slots)))
-    (check-documentation documentation)
-    (loop for defaulted-direct-superclass in defaulted-direct-superclasses
-          do (add-direct-subclass defaulted-direct-superclass class))
-    (loop for direct-slot-definition in direct-slot-definitions
-          do (create-readers-and-writers class direct-slot-definition))
-    (apply #'call-next-method
-           class
-           ;; We supply the DIRECT-DEFAULT-INITARGS argument
-           ;; explicitly, in case it was not given.  That way it
-           ;; becomes defaulted to the empty list in that case.
-           :direct-default-initargs direct-default-initargs
-           ;; We supply the DIRECT-SLOTS argument explicitly, in case
-           ;; it was not given.  That way it becomes defaulted to the
-           ;; empty list in that case.
-           :direct-slots direct-slot-definitions
-           ;; We supply the DOCUMENTATION argument explicitly, in case
-           ;; it was not given.  That way it becomes defaulted to NIL
-           ;; in that case.
-           :documentation documentation
-           ;; We supply the NAME argument explicitly, in case it was
-           ;; not given.  That way it becomes defaulted to NIL in that
-           ;; case.
-           :name name
-           :direct-superclasses defaulted-direct-superclasses
-           initargs)))
+              direct-superclasses)))
+    (let ((*existing-superclasses* '()))
+      (apply #'call-next-method
+             class
+             ;; We supply the DIRECT-DEFAULT-INITARGS argument
+             ;; explicitly, in case it was not given.  That way it
+             ;; becomes defaulted to the empty list in that case.
+             :direct-default-initargs direct-default-initargs
+             ;; We supply the DIRECT-SLOTS argument explicitly, in case
+             ;; it was not given.  That way it becomes defaulted to the
+             ;; empty list in that case.
+             :direct-slots direct-slots
+             ;; We supply the DOCUMENTATION argument explicitly, in case
+             ;; it was not given.  That way it becomes defaulted to NIL
+             ;; in that case.
+             :documentation documentation
+             ;; We supply the NAME argument explicitly, in case it was
+             ;; not given.  That way it becomes defaulted to NIL in that
+             ;; case.
+             :name name
+             :direct-superclasses defaulted-direct-superclasses
+             initargs))))
