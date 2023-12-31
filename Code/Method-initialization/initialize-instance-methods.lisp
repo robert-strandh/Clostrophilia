d99034eef2eeeed10de8040ea5b07e698a1c1527
Robert Strandh <robert.strandh@gmail.com>
2023-12-31 13:50:53 +0100

    Check and default method qualifiers.

--- Code/Method-initialization/initialize-instance-methods.lisp
+++ Code/Method-initialization/initialize-instance-methods.lisp
@@ -1,5 +1,14 @@
 (cl:in-package #:clostrophilia)
 
+(defun check-method-qualifiers (method-qualifiers)
+  (unless (ecclesia:proper-list-p method-qualifiers)
+    (error 'qualifiers-must-be-proper-list
+           :qualifiers method-qualifiers))
+  (loop for qualifier in method-qualifiers
+        unless (and (atom qualifier) (not (null qualifier)))
+          do (error 'qualifier-must-be-non-nil-atom
+                    :qualifier qualifier)))
+
 (defmethod initialize-instance :before
     ((method method)
      &key
@@ -7,7 +16,7 @@
        (specializers nil specializers-p)
        (function nil function-p)
      &allow-other-keys)
-  )
+  (check-method-qualifiers qualifiers))
 
 
 (defmethod initialize-instance :around
@@ -16,4 +25,6 @@
      &key
        (qualifiers '())
        )
-  )
+  (apply #'call-next-method
+         :qualifiers qualifiers
+         initargs))
