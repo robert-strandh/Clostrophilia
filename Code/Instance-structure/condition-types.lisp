3ab0e2f20b395459d9b3d0fb09b3d4a383b50367
Robert Strandh <robert.strandh@gmail.com>
2024-01-07 10:05:43 +0100

    Define condition type UNBOUND-SLOT.

--- Code/Instance-structure/condition-types.lisp
+++ Code/Instance-structure/condition-types.lisp
@@ -11,3 +11,14 @@
                       ~s"
                      (name condition)
                      (object condition)))))
+
+(define-condition unbound-slot (cell-error)
+  ((%instance :initarg :instance :reader unbound-slot-instance))
+  (:report (lambda (condition stream)
+             (format stream
+                     "The slot named:~@
+                      ~s~@
+                      is unbound in the object:~@
+                      ~s"
+                     (cell-error-name condition)
+                     (unbound-slot-instance condition)))))
