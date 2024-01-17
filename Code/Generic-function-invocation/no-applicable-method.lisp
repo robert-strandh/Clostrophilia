7e89fe94d69a29eb3481a752437897ab947c6e0c
Robert Strandh <robert.strandh@gmail.com>
2024-01-17 14:01:56 +0100

    Signal new condition NO-APPLICABLE-METHOD-ERROR.

--- Code/Generic-function-invocation/no-applicable-method.lisp
+++ Code/Generic-function-invocation/no-applicable-method.lisp
@@ -6,5 +6,5 @@
 
 (defmethod no-applicable-method
     ((generic-function generic-function) &rest function-arguments)
-  (error "no applicable method on generic function ~s with arguments: ~s"
-         generic-function function-arguments))
+  (error 'no-applicable-method-error
+         :function generic-function :arguments function-arguments))
