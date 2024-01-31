1f48e820c23bb9379568297a2d88913983fc1f30
Robert Strandh <robert.strandh@gmail.com>
2024-01-31 15:12:09 +0100

    Split code for FIND-METHOD-COMBINATION into two parts.

---
+++ Code/Method-combination/ensure-method-combination.lisp
@@ -0,0 +1,14 @@
+(cl:in-package #:clostrophilia)
+
+(defun ensure-method-combination (client template options)
+  (let* ((variant-determiner (variant-signature-determiner template))
+         (variant-signature (apply variant-determiner options)))
+    (loop for variant in (variants template)
+          do (when (equal (variant-signature variant)
+                          variant-signature)
+               (return-from find-method-combination variant)))
+    (let ((new-variant (make-instance 'method-combination
+                         :variant-signature variant-signature
+                         :template template)))
+      (push new-variant (variants template))
+      new-variant)))
--- Code/Method-combination/find-method-combination-defun.lisp
+++ Code/Method-combination/find-method-combination-defun.lisp
@@ -5,14 +5,4 @@
     (when (null template)
       (error 'unknown-method-combination
              :name name))
-    (let* ((variant-determiner (variant-signature-determiner template))
-           (variant-signature (apply variant-determiner options)))
-      (loop for variant in (variants template)
-            do (when (equal (variant-signature variant)
-                            variant-signature)
-                 (return-from find-method-combination variant)))
-      (let ((new-variant (make-instance 'method-combination
-                           :variant-signature variant-signature
-                           :template template)))
-        (push new-variant (variants template))
-        new-variant))))
+    (ensure-method-combination client template options)))
