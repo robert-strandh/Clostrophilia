c785e4f020934930e307f39fdfbbf81085967b97
Robert Strandh <robert.strandh@gmail.com>
2024-02-18 08:35:45 +0100

    Fix incorrect specializer.

--- Code/Class-initialization/default-superclasses.lisp
+++ Code/Class-initialization/default-superclasses.lisp
@@ -8,5 +8,5 @@
 (defmethod default-superclasses ((class standard-class))
   (list (find-class 'standard-object)))
 
-(defmethod default-superclasses (class)
+(defmethod default-superclasses ((class funcallable-standard-class)
   (list (find-class 'funcallable-standard-object)))
