53284c28feefc8f97e1da0920338e2c7f1a93d48
Robert Strandh <robert.strandh@gmail.com>
2024-03-18 17:33:51 +0100

    Remove code that belongs elsewhere.

--- Code/Standard-object-initialization/reinitialize-instance.lisp
+++ Code/Standard-object-initialization/reinitialize-instance.lisp
@@ -11,30 +11,3 @@
   ;; i.e., only assign values to slots that have explicit
   ;; initialization arguments in initargs.
   (apply #'shared-initialize instance () initargs))
-
-(defmethod reinitialize-instance :around
-    ((class real-class)
-     &rest initargs
-     &key (direct-superclasses '() direct-superclasses-p)
-     &allow-other-keys)
-  (let ((existing-direct-superclasses (class-direct-superclasses class)))
-    (check-direct-superclasses class direct-superclasses)
-    (when direct-superclasses-p
-      (when (null direct-superclasses)
-        (setf direct-superclasses (default-superclasses class)))
-      (loop with difference = (set-difference existing-direct-superclasses
-                                              direct-superclasses)
-            for removed-superclass in difference
-            do (remove-direct-subclass removed-superclass class)))
-    (if direct-superclasses-p
-        (apply #'call-next-method
-               class
-               :direct-superclasses direct-superclasses
-               initargs)
-        (call-next-method))
-    (when direct-superclasses-p
-      (loop with difference = (set-difference direct-superclasses
-                                              existing-direct-superclasses)
-            for added-superclass in difference
-            do (add-direct-subclass added-superclass class))))
-  class)
