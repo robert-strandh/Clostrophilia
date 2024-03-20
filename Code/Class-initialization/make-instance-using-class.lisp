88d426b3fcc8d70a9e472ed666777776b46b765b
Robert Strandh <robert.strandh@gmail.com>
2024-03-20 08:51:15 +0100

    New component containing definition of MAKE-INSTANCE-USING-CLASS.

---
+++ Code/Class-initialization/make-instance-using-class.lisp
@@ -0,0 +1,31 @@
+(cl:in-package #:clostrophilia)
+
+;;; This function is called by the method on MAKE-INSTANCE specialized
+;;; to CLASS.  Client code will have to define MAKE-INSTANCE
+;;; accordinly.  And Clostrophilia does not supply a definition for
+;;; ALLOCATE-INSTANCE, because object representation is the
+;;; responsibility of client code.
+
+;;; The AMOP says that ALLOCATE-INSTANCE checks whether the class is
+;;; finalized, and if not, calls FINALIZE-INHERITANCE.  However, the
+;;; INITARGS received by ALLOCATE-INSTANCE should be the defaulted
+;;; initargs, and computing the defaulted initargs requires the class
+;;; to be finalized.  A peek at PCL shows that the class is finalized
+;;; in MAKE-INSTANCE, before ALLOCATE-INSTANCE is called, which makes
+;;; more sense.  For that reason, we finalize the class here.
+
+(defun make-instance-using-class
+    (class &rest initargs &key &allow-other-keys)
+  (unless (class-finalized-p class)
+    (finalize-inheritance class))
+  ;; FIXME: check shape of initargs (proper, length is even, etc.).
+  (let ((defaulted-initargs initargs))
+    (loop with default = (load-time-value (list nil))
+          for (name form thunk) in (class-default-initargs class)
+          do (when (eq (getf initargs name default) default)
+               (setf defaulted-initargs
+                     (append defaulted-initargs
+                             (list name (funcall thunk))))))
+    (let ((instance (apply #'allocate-instance class defaulted-initargs)))
+      (apply #'initialize-instance instance defaulted-initargs)
+      instance)))
