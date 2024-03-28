a76c14ec7e6144e4432a1887e73aa5eda799a2b9
Robert Strandh <robert.strandh@gmail.com>
2024-03-28 08:39:10 +0100

    Add comment, and call SHARED-INITIALIZE-AUX to do most of the work.

--- Code/Standard-object-initialization/shared-initialize.lisp
+++ Code/Standard-object-initialization/shared-initialize.lisp
@@ -1,50 +1,31 @@
 (cl:in-package #:clostrophilia)
 
+;;; We assume this code is executed to create the generic function
+;;; SHARED-INITIALIZE and its method with refinement R.  Therefore,
+;;; the INSTANCE argument has refinement R+1.  CLASS-OF is called with
+;;; INSTANCE as its argument, so CLASS-OF has refinement R just like
+;;; SHARED-INITIALIZE, and CLASS-OF returns a class metaobject C of
+;;; refinement R since the class of an instance of refinement R+1 is
+;;; an object of refinement R.  However, SHARED-INITIALIZE does most
+;;; of its processing by accessing the slot metaobjects of C.  The
+;;; slot metaobjects of a class of refinement R also have refinement
+;;; R, so the functions that operate on those class metaobjects have
+;;; refinement R-1.  And the function CLASS-SLOTS that accesses the
+;;; slot metaobjects of C also has refinement R-1.
+;;;
+;;; We thus have a choice.  We can either call versions R-1 of the
+;;; functions that operate on C and its slot metaobjects directly from
+;;; here.  Or we can call version R-1 of an auxiliary function that
+;;; handles the part of SHARED-INITIALIZE that needs to access C and
+;;; its slot metaobjects.  We choose the second solution.
+
 (defgeneric shared-initialize
     (object slot-names &rest initargs &key &allow-other-keys))
 
 (defmethod shared-initialize
     ((instance standard-object) slot-names &rest initargs)
-  (let* ((class (class-of instance))
-         (slots (class-slots class)))
-    (loop for slot in slots
-          do ;; (multiple-value-bind (key value foundp)
-             ;;     ;; Find the first key/value pair in initargs where
-             ;;     ;; the key is one of the initargs of the slot.
-             ;;     (get-properties initargs (slot-definition-initargs slot))
-             ;;   (declare (ignore key))
-             ;;
-             ;; The following loop replaces the commented-out code
-             ;; above.  The reason is that MUTLIPE-VALUE-BIND might
-             ;; expand to MUTLIPLE-VALUE-CALL which in turn creates a
-             ;; closure, and allocating a closure triggers the
-             ;; object-initialization protocol, which lands us in an
-             ;; infinite recursion.
-             (let ((value nil)
-                   (foundp nil))
-               (loop with slot-initargs = (slot-definition-initargs slot)
-                     for (i v) on initargs by #'cddr
-                     when (member i slot-initargs)
-                       do (setf value v foundp t)
-                          (loop-finish))
-               (if foundp
-                   ;; Found an explicit initarg in initargs.
-                   ;; Initialize the slot from its value.
-                   (setf (slot-value-using-class class instance slot)
-                         value)
-                   ;; No explicit initarg found.
-                   (when (and (not (slot-boundp-using-class
-                                    class instance slot))
-                              (not (null (slot-definition-initfunction slot)))
-                              (or (eq slot-names t)
-                                  (member (slot-definition-name slot)
-                                          slot-names)))
-                     ;; Evaluate the initform by executing the
-                     ;; initfunction.
-                     (setf (slot-value-using-class class instance slot)
-                           (funcall (slot-definition-initfunction slot)))))))
-    ;; Store the class slots in the instance so that we can update the
-    ;; instance when the class changes.
-    (setf (standard-instance-access instance -1)
-          slots))
+  (apply #'shared-initialize-aux-1
+         instance (class-of instance) slot-names initargs)
   instance)
+
+; LocalWords:  metaobjects metaobject
