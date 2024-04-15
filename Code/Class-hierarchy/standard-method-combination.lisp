384c364ead6447135e3272365fc76a34e595ea77
Robert Strandh <robert.strandh@gmail.com>
2024-04-15 15:03:45 +0200

    Move slots to STANDARD-METHOD-COMBINATION.

--- Code/Class-hierarchy/standard-method-combination.lisp
+++ Code/Class-hierarchy/standard-method-combination.lisp
@@ -1,4 +1,9 @@
 (cl:in-package #:clostrophilia)
 
+(defgeneric template (method-combination))
+
+(defgeneric variant-signature (method-combination))
+
 (defclass standard-method-combination (method-combination)
-  ())
+  ((%template :initarg :template :reader template)
+   (%variant-signature :initarg :variant-signature :reader variant-signature)))
