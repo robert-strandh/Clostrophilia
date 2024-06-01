0f751718500a04c91f63f3a6ffa67e76c887b170
Robert Strandh <robert.strandh@gmail.com>
2024-06-01 16:12:38 +0200

    Define functions for updating and recomputing specializer profile.

--- Code/Generic-function-invocation/specializer-profile.lisp
+++ Code/Generic-function-invocation/specializer-profile.lisp
@@ -6,14 +6,19 @@
         for p in existing-profile
         collect (if (eq specializer class-t) p t)))
 
-(defun specializer-profile (generic-function)
-  (let* ((lambda-list (generic-function-lambda-list generic-function))
-         (canonicalized-lambda-list
-           (ecclesia:canonicalize-generic-function-lambda-list lambda-list))
-         (required (ecclesia:extract-required canonicalized-lambda-list))
-         (profile (make-list (length required) :initial-element nil)))
-    (loop for method in (generic-function-methods generic-function)
-          for specializers = (method-specializers method)
-          do (setf profile
-                   (compute-specializer-profile profile specializers)))
-    profile))
+;;; Update the specializer profile of a generic function according to
+;;; a list of specializers of a method.
+(defun update-specializer-profile (generic-function specializers)
+  (setf (specializer-profile generic-function)
+        (compute-specializer-profile
+         (specializer-profile generic-function) specializers)))
+
+;;; Compute a completely new specializer profile for a generic
+;;; function.
+(defun compute-and-set-specializer-profile (generic-function)
+  ;; Keep the length of the profile, but with all elements NIL.
+  (setf (specializer-profile generic-function)
+        (make-list (length (specializer-profile generic-function))))
+  (loop for method in (generic-function-methods generic-function)
+        for specializers = (method-specializers method)
+        do (update-specializer-profile generic-function specializers)))
