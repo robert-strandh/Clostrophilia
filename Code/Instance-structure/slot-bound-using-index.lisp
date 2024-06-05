d333a7ce9e9524d3033792cf36bbd75ecd9711b3
Robert Strandh <robert.strandh@gmail.com>
2024-06-05 14:16:13 +0200

    Remove unused functions.

--- Code/Instance-structure/slot-bound-using-index.lisp
+++ Code/Instance-structure/slot-bound-using-index.lisp
@@ -1,12 +1,3 @@
 (cl:in-package #:clostrophilia)
 
 (defvar +unbound-slot-value+)
-
-(defun slot-boundp-using-index (instance index)
-  (not (eq (standard-instance-access instance index)
-           +unbound-slot-value+)))
-
-(defun slot-makunbound-using-index (instance index)
-  (setf (standard-instance-access instance index)
-        +unbound-slot-value+)
-  nil)
