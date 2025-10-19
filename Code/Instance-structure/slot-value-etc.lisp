e1b4b13f21bf5a4e47e931390cf2da9f0c4fdb58
Robert Strandh <robert.strandh@gmail.com>
2025-10-19 09:36:38 +0200

    Remove -1 from symbol names.  Now handled during bootstrapping.

--- Code/Instance-structure/slot-value-etc.lisp
+++ Code/Instance-structure/slot-value-etc.lisp
@@ -1,14 +1,14 @@
 (cl:in-package #:clostrophilia)
 
 (defun slot-boundp (object slot-name)
-  (slot-boundp-using-only-class-1 (class-of object) object slot-name))
+  (slot-boundp-using-only-class (class-of object) object slot-name))
 
 (defun slot-value (object slot-name)
-  (slot-value-using-only-class-1 (class-of object) object slot-name))
+  (slot-value-using-only-class (class-of object) object slot-name))
 
 (defun (setf slot-value) (value object slot-name)
-  (setf (slot-value-using-only-class-1 (class-of object) object slot-name)
+  (setf (slot-value-using-only-class (class-of object) object slot-name)
         value))
 
 (defun slot-makunbound (object slot-name)
-  (slot-makunbound-using-only-class-1 (class-of object) object slot-name))
+  (slot-makunbound-using-only-class (class-of object) object slot-name))
