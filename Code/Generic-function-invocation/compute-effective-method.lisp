0e1e31a75ed0c8d249270ae00f59156ac5de2a51
Robert Strandh <robert.strandh@gmail.com>
2025-11-12 09:09:45 +0100

    Call method functions with :ALLOW-OTHER-KEYS T when required.

--- Code/Generic-function-invocation/compute-effective-method.lisp
+++ Code/Generic-function-invocation/compute-effective-method.lisp
@@ -13,6 +13,27 @@
                        (rest ,next-methods-var))))
        ,form)))
 
+;;; The standard says that methods with keyword parameters are called
+;;; "as if" :ALLOW-OTHER-METHODS T were passed in addition to other
+;;; keyword arguments.  The technique we use here is to literally add
+;;; :ALLOW-OTHER-METHODS T in that case, because we don't see any
+;;; other technique that will always work, given that the user is free
+;;; to create a method using MAKE-INSTANCE and passing it a :FUNCTION
+;;; argument that is any old existing function.  A much better
+;;; solution would be for methods to have an entry point that does not
+;;; parse keyword arguments, and to have the effective method call
+;;; this entry point when possible.  but this is a general library
+;;; that does not make any assumptions about the representation of
+;;; ordinary functions.
+
+(defun funcall-method-function
+    (method-function arguments lambda-list next-method)
+  (if (member '&key lambda-list)
+      (funcall method-function
+               (append arguments '(:allow-other-keys t))
+               next-method)
+      (funcall method-function arguments next-method)))
+
 (defun wrap-in-call-method-macrolet (form arguments-var)
   `(macrolet ((call-method (method &optional next-method-list)
                 (cond ((and (consp method)
@@ -23,9 +44,11 @@
                                    `(lambda ()
                                       ,(wrap-in-call-method-macrolet (second method) ',arguments-var)))))
                       ((not (consp method))
-                       `(funcall ,(method-function method)
-                                 ,',arguments-var
-                                 (list ,@next-method-list)))
+                       `(funcall-method-function
+                         ,(method-function method)
+                         ,',arguments-var
+                         ',(method-lambda-list method)
+                         (list ,@next-method-list)))
                       (t (error "Malformed argument to CALL-METHOD ~s" method)))))
      ,form))
 
